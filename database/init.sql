22771dd21250e4884a47965399873de9a252c789
hhasona <hhasona@gmail.com>
2021-09-08 11:58:43 +0300

    last one

--- database/init.sql
+++ database/init.sql
@@ -1,49 +1,64 @@
 BEGIN;
 
 DROP TABLE IF EXISTS newspapers, newspaper_pages, overlay_coords, admins, publishers CASCADE;
-CREATE TABLE publishers ( id SERIAL PRIMARY KEY, name VARCHAR(255) UNIQUE, logo VARCHAR(500) UNIQUE );
+
+CREATE TABLE publishers (  
+    id SERIAL PRIMARY KEY,  
+    name VARCHAR(255) UNIQUE,
+    logo VARCHAR(500) UNIQUE
+);
 
 INSERT INTO publishers (name, logo) VALUES ('aljazera', 'somelink.com');
-
 INSERT INTO publishers (name, logo) VALUES ('haarz', 'url.com');
 
-CREATE TABLE newspapers( id SERIAL UNIQUE, published_date DATE, publisher_id INTEGER, CONSTRAINT fk_publisher FOREIGN KEY(publisher_id) REFERENCES publishers(id), PRIMARY KEY( publisher_id, published_date ) );
+CREATE TABLE newspapers(
+    id SERIAL UNIQUE,  
+    published_date DATE,
+    publisher_id INTEGER,  
+    CONSTRAINT fk_publisher FOREIGN KEY(publisher_id) REFERENCES publishers(id),
+    PRIMARY KEY(
+        publisher_id,
+        published_date
+    )
+);
 
 INSERT INTO newspapers (published_date, publisher_id) VALUES ('2021-09-05', '2');
 
-CREATE TABLE newspaper_pages( id SERIAL PRIMARY KEY, newspaper_id INTEGER REFERENCES newspapers(id), page_number INTEGER, name VARCHAR(255) );
+CREATE TABLE newspaper_pages(
+    id SERIAL PRIMARY KEY,  
+    newspaper_id INTEGER REFERENCES newspapers(id),
+    page_number INTEGER,
+    name VARCHAR(255) 
+);
 
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '1', 'HAREEAT_page_1');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '2', 'HAREEAT_page_2');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '3', 'HAREEAT_page_3');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '4', 'HAREEAT_page_4');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '5', 'HAREEAT_page_5');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '6', 'HAREEAT_page_6');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '7', 'HAREEAT_page_7');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '8', 'HAREEAT_page_8');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '9', 'HAREEAT_page_9');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '10', 'HAREEAT_page_10');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '11', 'HAREEAT_page_11');
-
 INSERT INTO newspaper_pages (newspaper_id, page_number, name) VALUES ('1', '12', 'HAREEAT_page_12');
 
-CREATE TABLE overlay_coords( id SERIAL PRIMARY KEY, newspaper_id INTEGER REFERENCES newspapers(id), coords TEXT[] );
+
+CREATE TABLE overlay_coords(
+    id SERIAL PRIMARY KEY,  
+    newspaper_id INTEGER REFERENCES newspapers(id),
+    coords TEXT
+);
+INSERT INTO overlay_coords (newspaper_id,coords) VALUES ('1','[{"x":4756.60681369538,"y":330.51214148731304,"width":378.2821562709105,"height":287.21052721097345,"degrees":0},{"x":5330.046355699557,"y":32.4140969014465,"width":176.0542458985219,"height":412.9440855428983,"degrees":0},{"x":5503.883110580155,"y":454.2190584381016,"width":185.9329866218177,"height":343.8565170254213,"degrees":0}]');
+
+CREATE TABLE admins(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+);
 
 
-INSERT INTO overlay_coords（newspaper_id ,coords） VALUES （'1',
-'[{"x":4756.60681369538 "y":330.51214148731304 "width":378.2821562709105 "height":287.21052721097345 "degrees":0} {"x":5330.046355699557 "y":32.4140969014465 "width":176.0542458985219 "height":412.9440855428983 "degrees":0} {"x":5503.883110580155 "y":454.2190584381016 "width":185.9329866218177 "height":343.8565170254213 "degrees":0}]'）;
 
-CREATE TABLE admins( id SERIAL PRIMARY KEY, email VARCHAR(255) UNIQUE, password VARCHAR(255), created_at DATE DEFAULT CURRENT_TIMESTAMP );
-
--- INSERT INTO admins（email password） VALUES （'librarian@example.com' '$2b$12$dc1CMMpXRv1giih./ccEgOzaWD/rE2apJOLs7bxDXd7SXWzGhUv5S'）; 
 COMMIT;
