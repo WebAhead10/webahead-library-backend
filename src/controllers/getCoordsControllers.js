caa4c41f366d108b9f18373f8693dc9a89ac8fbe
noor <n.swa3ed.swa3ed@gmail.com>
2021-09-13 22:24:25 +0300

    view news paper - no major changes

--- src/controllers/getCoordsControllers.js
+++ src/controllers/getCoordsControllers.js
@@ -2,16 +2,15 @@ import db from "../../database/connection"
 
 export const getCoords = async (req, res) => {
   const { id } = req.params
-  console.log(id)
   try {
     const result = await db.query(
-      `SELECT coords FROM overlay_coords WHERE id=$1`,
+      `SELECT id , coords FROM overlay_coords WHERE newspaper_id = $1`,
       [id]
     )
 
     res.send({
       success: true,
-      pages: JSON.parse(result.rows[0].coords),
+      coordsData: result.rows,
     })
   } catch (error) {
     console.log(error)
