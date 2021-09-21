75412c5590d8f3e419a04a97f279218d2fbc7891
Adan-Sa <adan.saada11@gmail.com>
2021-09-21 16:20:34 +0300

    updating deleteTag

--- src/controllers/autocomplete.js
+++ src/controllers/autocomplete.js
@@ -2,7 +2,7 @@ const db = require("../../database/connection");
 
 function autocomplete(req, res) {
     const input = req.body;
-    db.query(`SELECT * FROM tags WHERE tag_name LIKE '%${input.tag_name}%'`)
+    db.query(`SELECT * FROM tags WHERE tag_name LIKE '%${input.tag_name}'`)
         .then((result) => {
             res.send({
                 success: true,
