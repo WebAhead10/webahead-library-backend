06feb782ee3c34e5066c37cf60f9e9249ff4908e
--replace-all <mario.saliba98@gmail.com>
2021-09-21 10:29:38 +0300

    updates the tags feature

--- src/controllers/addTag.js
+++ src/controllers/addTag.js
@@ -1,35 +1,37 @@
-const db = require("../../database/connection");
+const db = require("../../database/connection")
 
 function addTag(req, res) {
-    const input = req.body;
-    db.query(`SELECT * FROM tags WHERE tag_name=$1`, [input.tag_name])
-        .then((result) => {
-            //Checking if the tags table has the new added tag, if not we will add it
-            if (!result.rows.length) {
-                //Adding the tag to the tags table in the database
-                db.query(`INSERT INTO tags(tag_name) VALUES ($1) RETURNING id`, [input.tag_name])
-                    .then((result) => {
-                        res.send({
-                            success: true,
-                            tag_id: result.rows[0].id
-                        });
-                    })
+  const input = req.body.tag
+  // { tag: 'sport' }
 
-            } else {
-                res.send({
-                    success: false,
-                    message: "Tag is already exist"
-                })
-            }
+  db.query(`SELECT * FROM tags WHERE tag_name=$1`, [input.tag_name])
+    .then((result) => {
+      //Checking if the tags table has the new added tag, if not we will add it
+      if (!result.rows.length) {
+        //Adding the tag to the tags table in the database
+        db.query(`INSERT INTO tags(tag_name) VALUES ($1) RETURNING id`, [
+          input,
+        ]).then((result) => {
+          res.send({
+            success: true,
+            tag_id: result.rows[0].id,
+          })
         })
-        .catch(error => {
-            res.send({
-                success: false,
-                message: "Something went wrong"
-            });
+      } else {
+        res.send({
+          success: false,
+          message: "Tag is already exist",
         })
+      }
+    })
+    .catch((error) => {
+      res.send({
+        success: false,
+        message: "Something went wrong",
+      })
+    })
 }
 
 module.exports = {
-    addTag
+  addTag,
 }
