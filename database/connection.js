482acbf48f87a63881e6dc14ecf083e38d56ce85
--replace-all <mario.saliba98@gmail.com>
2021-09-22 13:02:57 +0300

    .

--- database/connection.js
+++ database/connection.js
@@ -4,7 +4,7 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 const connectionString = process.env.DATABASE_URL
-const isProd = process.env?.NODE_ENV?.includes("compute.amazonaws.com")
+const isProd = connectionString.includes("compute.amazonaws.com")
 
 const db = new pg.Pool({
   connectionString,
