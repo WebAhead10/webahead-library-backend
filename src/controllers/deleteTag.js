39a69eaef94d8256602cd76ee85bc0b9647a7baf
--replace-all <mario.saliba98@gmail.com>
2021-09-22 20:50:30 +0300

    fix delete

--- src/controllers/deleteTag.js
+++ src/controllers/deleteTag.js
@@ -1,22 +1,21 @@
-const db = require("../../database/connection");
+const db = require("../../database/connection")
 
 function deleteTag(req, res) {
-  const input = req.body.tag_name;
-  console.log(input);
-  db.query(`DELETE FROM tags WHERE tag_name=$1`, [input])
+  console.log(req.body)
+  db.query(`DELETE FROM tags WHERE id=$1`, [req.body.id])
     .then(() => {
       res.send({
         success: true,
-      });
+      })
     })
     .catch((error) => {
       res.send({
         success: false,
         message: "Something went wrong",
-      });
-    });
+      })
+    })
 }
 
 module.exports = {
   deleteTag,
-};
+}
