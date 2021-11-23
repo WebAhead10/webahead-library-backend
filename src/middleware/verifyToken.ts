d0dfa1c737cfc20f376a4d33e51d1d0913ffedf5
kassim <kassimbashir@gmail.com>
2021-11-23 11:14:52 +0200

    fixed changed requested by s. mario Relates #11

--- src/middleware/verifyToken.ts
+++ src/middleware/verifyToken.ts
@@ -6,7 +6,7 @@ import { catchAsync } from '../utils'
 import db from '../database/connection'
 const secret = config.jwtSecret
 // TOKEN FORMAT: authorization: Bearer <access_token>
-const verifyToken = (
+const verifyToken = async(
     req: Request,
     res: Response,
     next: NextFunction
@@ -19,27 +19,25 @@ const verifyToken = (
             // split at the space and get token from array
             const token = header.split(' ')[1];
            // verify token
-           jwt.verify(token,secret,async (err,data)=>{
-               if(err){
-                   console.log(err);
-                   res.sendStatus(403);
-               }else{
-                   if(data){
-                    const userId = data.id;
-                     /** get user from DB using the id we got from verifying token */
-                    const result = await db.query('SELECT * FROM users WHERE id = $1', [userId])
-                    req.user = result.rows[0];
-                   }
-                 
-                next();
-               }
-           })
-            
-        }else{
+           try{
+            const decoded = <any>jwt.verify(token,secret)
+            console.log("decoded",decoded)
+            const userId = decoded.id;
+            /** get user from DB using the id we got from verifying token */
+           const result = await db.query('SELECT * FROM users WHERE id = $1', [userId])
+           req.user = result.rows[0];
+             next();
+           }catch{
             // forbidden
             res.sendStatus(403);
+           }
+          
+           }
+           
         }
+            
+        
    
-}
+
 
 export { verifyToken }
